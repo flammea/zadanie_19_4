f3ad13416c04e88b13d7608ab2c5e401c6721917
Beata Markunas <beata.markunas@gmail.com>
2018-05-18 07:27:32 +0200

    Update actions and main reducer

--- reducer.js
+++ reducer.js
@@ -1,32 +1,13 @@
-import {ADD_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {REMOVE_COMMENT} from './actions';
-import {THUMB_UP_COMMENT} from './actions';
-import {THUMB_DOWN_COMMENT} from './actions';
-
+import {combineReducers} from 'redux';
+import comments from './comments';
 
 const initialState = {
-    comments: [],
-    users: []
+	comments: [],
+	users: []
 };
 
-function reducer(state = initialState, action) {
-    return state;
 
-
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    text: action.text,
-                    votes: 0
-                }
-                , ...state]
-            })
-        default:
-            return state;
-    }
-}
+const app = combineReducers({
+		comments,
+		users
+});
